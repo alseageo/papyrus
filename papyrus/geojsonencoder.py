ec221442738d033d6c46996d65cd3b224644a1e7
tsemerad <tannersemerad@gmail.com>
2013-12-17 09:09:51 -0800

    added support for UUID columns

--- papyrus/geojsonencoder.py
+++ papyrus/geojsonencoder.py
@@ -5,19 +5,22 @@ from sqlalchemy.ext.associationproxy import _AssociationList
 
 from geojson import dumps as _dumps
 from geojson.codec import PyGFPEncoder
+from uuid import UUID
 
 
 class GeoJSONEncoder(PyGFPEncoder):
     # SQLAlchemy's Reflecting Tables mechanism uses decimal.Decimal
-    # for numeric columns and datetime.date for dates. simplejson
-    # doesn't deal with these types. This class provides a simple
-    # encoder to deal with objects of these types.
+    # for numeric columns, datetime.date for dates, and uuid.UUID for
+    # PostgreSQL UUID columns. simplejson doesn't deal with these types.
+    # This class provides a simple encoder to deal with objects of these types.
 
     def default(self, obj):
         if isinstance(obj, (datetime.date, datetime.datetime)):
             return obj.isoformat()
         if isinstance(obj, _AssociationList):
             return list(obj)
+        if isinstance(obj, UUID):
+            return obj.hex
         return PyGFPEncoder.default(self, obj)
 
 
